cf43f1ae89c788131495bee34c528001cac015fc
Rishit <rishitbutola10@gmail.com>
2024-10-02 21:10:50 +0530

    navigation done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navbar.jsx'
+import ThemeButton from './components/themebutton.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <ThemeButton />
       
     </div>
   );
